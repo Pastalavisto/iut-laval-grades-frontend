cb60630e4759984f2a1da433579fffa9cc089457
Pastalavisto <nathan.hameau17@gmail.com>
2025-01-01 17:21:51 +0100

    Backend

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Login from './pages/Login/index.tsx';
-import { ProtectedRoute } from './components/ProtectedRoutes.tsx';
+import { ProtectedRoutes } from './components/ProtectedRoutes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<ProtectedRoute><App/></ProtectedRoute>} />
-        <Route path='/login' element={<Login/>} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<App />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
