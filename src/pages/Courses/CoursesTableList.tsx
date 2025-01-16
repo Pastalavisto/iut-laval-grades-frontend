178922ba086fcb4291c24ad6b9c0db235d6a84e7
Pastalavisto <96241542+Pastalavisto@users.noreply.github.com>
2025-01-16 19:53:56 +0100

    feat : v1 of courses list, we can add, and view courses

---
+++ src/pages/Courses/CoursesTableList.tsx
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+import { courseAddformSchema } from './AddCourseForm';
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+
+interface CoursesTableListProps {
+  courses: z.infer<typeof courseAddformSchema>[]; //Array of courses
+}
+
+export default function CoursesTableList(props: CoursesTableListProps) {
+  const courses = props.courses;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Code</TableHead>
+          <TableHead>Nom</TableHead>
+          <TableHead>Crédits ECTS</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {courses.map((course) => (
+          <TableRow key={course.code + course.name}>
+            <TableCell>{course.code}</TableCell>
+            <TableCell>{course.name}</TableCell>
+            <TableCell>{course.credits}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
