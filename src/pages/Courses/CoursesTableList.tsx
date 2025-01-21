5e9064cf93b8d521e0d339238732491c227418dd
Pastalavisto <nathan.hameau17@gmail.com>
2025-01-21 02:40:16 +0100

    feat : delete course

--- src/pages/Courses/CoursesTableList.tsx
+++ src/pages/Courses/CoursesTableList.tsx
@@ -2,14 +2,22 @@ import { z } from 'zod';
 
 import { courseAddformSchema } from './AddCourseForm';
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+import { TrashIcon } from '@heroicons/react/16/solid';
+
+export interface Course {
+  id: string;
+  code: string;
+  name: string;
+  credits: number;
+  description: string;
+}
 
 interface CoursesTableListProps {
-  courses: z.infer<typeof courseAddformSchema>[]; //Array of courses
+  courses: Course[]; //List of courses
+  onDeleteGrade: (id: string) => void; //Function to delete a course
 }
 
 export default function CoursesTableList(props: CoursesTableListProps) {
-  const courses = props.courses;
-
   return (
     <Table>
       <TableHeader>
@@ -18,15 +26,19 @@ export default function CoursesTableList(props: CoursesTableListProps) {
           <TableHead>Nom</TableHead>
           <TableHead>Crédits ECTS</TableHead>
           <TableHead>Description</TableHead>
+          <TableHead>Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {courses.map((course) => (
+        {props.courses.map((course) => (
           <TableRow key={course.code + course.name}>
             <TableCell>{course.code}</TableCell>
             <TableCell>{course.name}</TableCell>
             <TableCell>{course.credits}</TableCell>
             <TableCell>{course.description}</TableCell>
+            <TableCell>
+              <TrashIcon width={20} className="cursor-pointer" onClick={() => props.onDeleteGrade(course.id)} />
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
